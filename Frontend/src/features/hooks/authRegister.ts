c51416965df9f11845d0c3ec8c8a6a726447db3a
Baguser7 <bagushendrawan01@gmail.com>
2024-06-21 14:40:46 +0700

    fe: fix authregister

--- Frontend/src/features/hooks/authRegister.ts
+++ Frontend/src/features/hooks/authRegister.ts
@@ -43,7 +43,7 @@ export const useRegisterForm = () => {
             }
           } catch (error : any) {
             toast({
-              title: error,
+              title: error.response.data,
               status: "error",
               duration: 3000,
               isClosable: true,
